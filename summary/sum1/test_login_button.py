2d07e9f89bf8aca8b5eb290b45af0b2188f1e636
Kateryna <katua.korzyn@gmail.com>
2026-02-13 11:46:32 +0100

    update sum1

--- summary/sum1/test_login_button.py
+++ summary/sum1/test_login_button.py
@@ -11,3 +11,16 @@ def test_login_button(checker):
     assert checker.get_label() == "Login"
 
 
+def test_button_is_enable_by_default(checker):
+    assert checker.is_enabled() is True
+
+
+def test_button_is_disabled(checker):
+    checker.disable()
+    assert checker.is_enabled() is False
+
+
+def test_button_status_chek(checker):
+    checker.disable()
+    checker.enable()
+    assert checker.is_enabled() is True
