368738bf925e23ffb3969c14861dd5ab6277d029
Kateryna <katua.korzyn@gmail.com>
2026-02-18 19:43:17 +0100

    added a new task in summary

--- summary/sum2/s.py
+++ summary/sum2/s.py
@@ -12,18 +12,46 @@ def driver():
     driver.quit()
 
 
-def test_berlin(driver):
+# def test_login(driver):
+#     driver.get("https://the-internet.herokuapp.com/login")
+#     but_username = driver.find_element(By.ID, "username")
+#     but_username.send_keys("tomsmith")
+#     # sleep(2)
+#
+#     but_password = driver.find_element(By.ID, "password")
+#     but_password.send_keys("SuperSecretPassword!")
+#     # sleep(5)
+#
+#     but_submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+#     but_submit.click()
+#     sleep(2)
+#
+#     assert "https://the-internet.herokuapp.com/secure" in driver.current_url
+#
+#     text_info = driver.find_element(By.ID, "flash")
+#     # assert text_info.text == "You logged into a secure area!\n×"
+#     assert "You logged into a secure area!" in text_info.text
+
+
+def test_wrong_login(driver):
     driver.get("https://the-internet.herokuapp.com/login")
     but_username = driver.find_element(By.ID, "username")
     but_username.send_keys("tomsmith")
-    # sleep(2)
-
-    but_password = driver.find_element(By.ID, "password")
-    but_password.send_keys("SuperSecretPassword!")
-    # sleep(5)
-
-    but_submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    but_submit.click()
     sleep(2)
 
-    assert "https://the-internet.herokuapp.com/secure" in driver.current_url
+    but_password = driver.find_element(By.ID, "password")
+    but_password.send_keys("qwerty")
+    sleep(2)
+
+    button_sub = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button_sub.click()
+    sleep(2)
+    assert "https://the-internet.herokuapp.com/login" in driver.current_url
+
+
+
+
+
+
+
+
